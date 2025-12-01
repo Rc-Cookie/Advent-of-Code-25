a64643245b0b1a8210d273042450b5af419f2fff
RcCookie <rccookie@mail.de>
2025-12-01 10:53:09 +0100

    Day 1

--- src/main/java/de/rccookie/aoc/aoc25/Solution1.java
+++ src/main/java/de/rccookie/aoc/aoc25/Solution1.java
@@ -3,8 +3,33 @@ package de.rccookie.aoc.aoc25;
 import de.rccookie.aoc.aoc25.util.FastSolution;
 
 public class Solution1 extends FastSolution {
+
+    private long parse(String line) {
+        return (line.startsWith("L") ? -1 : 1) * Long.parseLong(line.substring(1));
+    }
+
     @Override
     public Object task1() {
-        return null;
+        long count = 0;
+        long pos = 50;
+        for(long offset : lines.stream().map(this::parse)) {
+            pos = Math.floorMod(pos + offset, 100);
+            if(pos == 0)
+                count++;
+        }
+        return count;
+    }
+
+    @Override
+    public Object task2() {
+        long count = 0;
+        long pos = 50;
+        for(long offset : lines.stream().map(this::parse)) {
+            if(offset >= 0)
+                count += (pos + offset) / 100;
+            else count += ((100 - pos) % 100 - offset) / 100;
+            pos = Math.floorMod(pos + offset, 100);
+        }
+        return count;
     }
 }
