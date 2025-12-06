d05a125e00feef469deff6e05de9619ade440383
RcCookie <rccookie@mail.de>
2025-12-06 19:59:09 +0100

    Simplify with Grid

--- src/main/java/de/rccookie/aoc/aoc25/Solution4.java
+++ src/main/java/de/rccookie/aoc/aoc25/Solution4.java
@@ -1,43 +1,28 @@
 package de.rccookie.aoc.aoc25;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import de.rccookie.aoc.Grid;
 import de.rccookie.aoc.aoc25.util.FastSolution;
-import de.rccookie.math.constInt2;
+import de.rccookie.math.int2;
 
 public class Solution4 extends FastSolution {
 
-    List<constInt2> getAccessible() {
-        List<constInt2> accessible = new ArrayList<>();
-        for(constInt2 p : size) {
-            if(charAt(p) != '@')
-                continue;
-            int adj = 0;
-            for(constInt2 o : ADJ8)
-                if(charAt(p.added(o), ' ') == '@')
-                    adj++;
-            if(adj < 4)
-                accessible.add(p);
-        }
-        return accessible;
+    List<int2> getAccessible(Grid grid) {
+        return grid.stream('@').filter(p -> grid.adjPos8(p, '@').length < 4);
     }
 
     @Override
     public Object task1() {
-        return getAccessible().size();
+        return getAccessible(new Grid(input)).size();
     }
 
     @Override
     public Object task2() {
-        int total = 0;
-        while(true) {
-            List<constInt2> accessible = getAccessible();
-            if(accessible.isEmpty())
-                return total;
-            total += accessible.size();
-            for(constInt2 p : accessible)
-                charTable[p.y()][p.x()] = ' ';
-        }
+        Grid grid = new Grid(input);
+        int change, total = 0;
+        do total += change = grid.set(getAccessible(grid), '.');
+        while(change != 0);
+        return total;
     }
 }
