c4a8339d0c9cd8bc3b02f08ce8ce8aa54da553ac
RcCookie <rccookie@mail.de>
2025-12-06 21:24:06 +0100

    Use predefined grid instance

--- src/main/java/de/rccookie/aoc/aoc25/Solution4.java
+++ src/main/java/de/rccookie/aoc/aoc25/Solution4.java
@@ -2,26 +2,24 @@ package de.rccookie.aoc.aoc25;
 
 import java.util.List;
 
-import de.rccookie.aoc.Grid;
 import de.rccookie.aoc.aoc25.util.FastSolution;
 import de.rccookie.math.int2;
 
 public class Solution4 extends FastSolution {
 
-    List<int2> getAccessible(Grid grid) {
+    List<int2> accessible() {
         return grid.stream('@').filter(p -> grid.adjPos8(p, '@').length < 4);
     }
 
     @Override
     public Object task1() {
-        return getAccessible(new Grid(input)).size();
+        return accessible().size();
     }
 
     @Override
     public Object task2() {
-        Grid grid = new Grid(input);
         int change, total = 0;
-        do total += change = grid.set(getAccessible(grid), '.');
+        do total += change = grid.set(accessible(), '.');
         while(change != 0);
         return total;
     }
