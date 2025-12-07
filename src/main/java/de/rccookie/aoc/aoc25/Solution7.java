bd3848ed8039ed92acf9372af3194ac71c94ef16
RcCookie <rccookie@mail.de>
2025-12-07 06:28:12 +0100

    Day 7

---
+++ src/main/java/de/rccookie/aoc/aoc25/Solution7.java
@@ -0,0 +1,41 @@
+package de.rccookie.aoc.aoc25;
+
+import de.rccookie.aoc.aoc25.util.FastSolution;
+import de.rccookie.math.Mathf;
+import de.rccookie.math.constInt2;
+
+public class Solution7 extends FastSolution {
+
+    long[] beams;
+    int splits = 0;
+
+    @Override
+    public void load() {
+        constInt2 S = grid.find('S');
+        beams = new long[grid.width];
+        beams[S.x()] = 1;
+
+        for(int y=S.y()+1; y<grid.height; y++) {
+            long[] newBeams = new long[grid.width];
+            for(int x=0; x<grid.width; x++) {
+                if(grid.charAt(x,y) == '^') {
+                    newBeams[x-1] += beams[x];
+                    newBeams[x+1] += beams[x];
+                    splits += Mathf.sign(beams[x]);
+                }
+                else newBeams[x] += beams[x];
+            }
+            beams = newBeams;
+        }
+    }
+
+    @Override
+    public Object task1() {
+        return splits;
+    }
+
+    @Override
+    public Object task2() {
+        return Mathf.sumL(beams);
+    }
+}
