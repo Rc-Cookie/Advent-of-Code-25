ae708f60a090e7432e604b94a86650397a7c7cd1
RcCookie <rccookie@mail.de>
2025-12-12 20:33:23 +0100

    Day 10 Part 2 + optimize Part 1

--- src/main/java/de/rccookie/aoc/aoc25/Solution10.java
+++ src/main/java/de/rccookie/aoc/aoc25/Solution10.java
@@ -1,48 +1,134 @@
 package de.rccookie.aoc.aoc25;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import de.rccookie.aoc.aoc25.util.FastSolution;
-import de.rccookie.math.constFloatMxN;
-import de.rccookie.math.constIntN;
-import de.rccookie.math.floatMxN;
-import de.rccookie.math.intN;
-import de.rccookie.util.Console;
-import de.rccookie.util.text.TableRenderer;
+import de.rccookie.aoc.aoc25.util.IntArrayList;
 
 public class Solution10 extends FastSolution {
+
     @Override
     public Object task1() {
         return sum(l -> {
             String[] parts = l.split("\\s+");
-            long target = Long.parseLong(parts[0].substring(1, parts[0].length() - 1).replace('.', '0').replace('#', '1'), 2);
-            long[] buttons = new long[parts.length - 2];
+            int target = Integer.parseInt(parts[0].substring(1, parts[0].length() - 1).replace('.', '0').replace('#', '1'), 2);
+            int[] buttons = new int[parts.length - 2];
             for(int i=0; i<buttons.length; i++)
                 for(int b : parseInts(parts[i+1]))
-                    buttons[i] |= 1L << (parts[0].length() - b - 3);
+                    buttons[i] |= 1 << (parts[0].length() - b - 3);
 
-            Map<Long, Integer> cache = new HashMap<>();
-
-            return count(target, buttons, 0, cache);
+            int[] combinations = combinations(target, buttons);
+            int min = Integer.MAX_VALUE;
+            for(int c : combinations)
+                min = Math.min(min, Integer.bitCount(c));
+            return min;
         });
     }
 
-    private int count(long target, long[] buttons, long usedButtons, Map<Long, Integer> cache) {
+    /**
+     * Constructs all combinations of pressing a button 1 or 0 times to achieve the given
+     * target mask, with toggling (i.e. pressing twice turns the state back off).
+     */
+    private static int[] combinations(int target, int[] buttons) {
+        IntArrayList combinations = new IntArrayList();
         if(target == 0)
-            return 0;
-        long key = target | (usedButtons << 32);
-        Integer cached = cache.get(key);
-        if(cached != null)
-            return cached;
-
-        int count = Integer.MAX_VALUE;
-        for(int i=0; i<buttons.length; i++) {
-            if((usedButtons & 1L << i) == 0) {
-                count = Math.min(count, (int) Math.min(Integer.MAX_VALUE, 1L + count(target ^ buttons[i], buttons, usedButtons | 1L << i, cache)));
-            }
+            combinations.add(0);
+        // Use Gray Code iteration order; that way we only need a single update per iteration
+        // rather than reconstructing the whole result pattern each time
+        int end = 1 << buttons.length;
+        int c = 0, pat = 0;
+        for(int i=1; i<end; i++) {
+            int l = Integer.lowestOneBit(i);
+            c ^= l;
+            pat ^= buttons[Integer.numberOfTrailingZeros(l)];
+            if(pat == target)
+                combinations.add(c);
         }
-        cache.put(key, count);
-        return count;
+        return combinations.toArrayNoCopy();
+    }
+
+    /**
+     * Returns in the n-th bit a 1 iff the n-th value is an odd number.
+     */
+    private static int parity(int[] values) {
+        assert values.length <= 32;
+        int par = 0;
+        for(int i=0; i<values.length; i++)
+            par |= (values[i] & 1) << i;
+        return par;
+    }
+
+    @Override
+    public Object task2() {
+        return sum(l -> {
+            String[] parts = l.split("\\s+");
+
+            int[][] buttons = new int[parts.length - 2][];
+            int[] buttonMasks = new int[parts.length - 2];
+            for(int i=0; i<buttonMasks.length; i++)
+                for(int b : buttons[i] = parseInts(parts[i+1]))
+                    buttonMasks[i] |= 1 << b;
+
+
+            int[] target = parseInts(parts[parts.length - 1]);
+
+            return count(target, buttonMasks, buttons, new int[1 << target.length][]);
+        });
+    }
+
+    /**
+     * Returns the minimum number of button presses needed to achieve the specified target
+     * values by pressing any combination of the given buttons.
+     *
+     * @param target The target values
+     * @param buttonMasks A bitmask per button, where the i-th bit indicates whether the button
+     *                    increases the i-th value
+     * @param buttons The same buttons, but for each button, a list of indices whose value this
+     *                button increases
+     * @param combinationsCache Cache for {@link #combinations(int, int[])}, where the i-th
+     *                          element is <code>combinations(i, buttonMasks)</code>, or null
+     *                          if not yet populated
+     * @return The minimum number of button presses needed to achieve the given pattern
+     * @implNote Based on the algorithm proposed <a href="https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/">here</a>
+     */
+    private static int count(int[] target, int[] buttonMasks, int[][] buttons, int[][] combinationsCache) {
+
+        int parity = parity(target);
+
+        int[] combinations = combinationsCache[parity];
+        if(combinations == null)
+            combinations = combinationsCache[parity] = combinations(parity, buttonMasks);
+
+        int[] remaining = new int[target.length];
+
+        int min = Integer.MAX_VALUE;
+        outer: for(int c : combinations) {
+            int count = Integer.bitCount(c);
+            System.arraycopy(target, 0, remaining, 0, target.length);
+
+            for(int i=0; i<buttonMasks.length; i++)
+                if((c & 1 << i) != 0 && !pressButton(buttons[i], remaining))
+                    continue outer;
+
+            boolean zero = true;
+            for(int i=0; i<remaining.length; i++)
+                zero &= (remaining[i] >>>= 1) == 0;
+
+            int additional = zero ? 0 : count(remaining, buttonMasks, buttons, combinationsCache);
+            if(additional < Integer.MAX_VALUE && count + (additional << 1) < min)
+                min = count + (additional << 1);
+        }
+        return min;
+    }
+
+    /**
+     * Decreases all values controlled by the given button by 1.
+     *
+     * @return <code>false</code> if any value becomes negative from pressing
+     *         the button, <code>true</code> otherwise
+     */
+    private static boolean pressButton(int[] button, int[] remaining) {
+        for(int b : button)
+            if(--remaining[b] < 0)
+                return false;
+        return true;
     }
 }
